bfecb2c0450177d9d7c4917d663f7dd35b3c6efb
Anton Te <te.anton@gmail.com>
2014-09-10 08:12:24 -0700

    Error handling

--- read_file/read_file.cpp
+++ read_file/read_file.cpp
@@ -1,10 +1,13 @@
 #include "read_file.hpp"
 #include <sstream>
 #include <fstream>
+#include <stdexcept>
 
 std::string readFile(std::string fileName)
 {
   std::ifstream f(fileName);
+  if (!f.good())
+    throw std::runtime_error("Could not open the file: " + fileName);
   std::stringstream strm;
   strm << f.rdbuf();
   return strm.str();
