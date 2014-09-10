6a6059115c4fcdf79c4fba1eda09198a4cd6285e
Anton Te <te.anton@gmail.com>
2014-09-10 08:24:42 -0700

    Add ms_time library

---
+++ ms_time/ms_time.cpp
@@ -0,0 +1,19 @@
+#include "ms_time.hpp"
+#include <sys/time.h>
+#include <cstring>
+#include <stdexcept>
+#include <sstream>
+
+uint64_t msTime()
+{
+  struct timeval tv;
+  if (gettimeofday(&tv, nullptr) != 0)
+  {
+    auto errCode = errno;
+    std::ostringstream strm;
+    strm << "Error: [" << errCode << "] " << strerror(errCode);
+    throw std::runtime_error(strm.str());
+  }
+  return tv.tv_sec * 1000 + tv.tv_usec;
+}
+
---
+++ ms_time/ms_time.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <cstdint>
+
+uint64_t msTime();
